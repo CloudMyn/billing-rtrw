08d4e501aea10d151f47ce170f2bb2df22a48688
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2026-09-15 09:27:47 +0700

    build(android): upgrade compileSdk and targetSdk to 36 for Google Play requirement

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.alijaya.customer"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.alijaya.customer"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 4
         versionName = "1.2.2"
 
